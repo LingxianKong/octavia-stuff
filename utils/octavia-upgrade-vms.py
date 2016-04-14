62caf0ed3bfa05ff73ceeb07d32aa270985b0b0e
Lingxian Kong <anlin.kong@gmail.com>
2016-04-14 23:52:40 +1200

    Update script for upgrading amphorae vms

--- utils/octavia-upgrade-vms.py
+++ utils/octavia-upgrade-vms.py
@@ -13,8 +13,6 @@
 #    under the License.
 
 import argparse
-import functools
-from multiprocessing.dummy import Pool as ThreadPool
 import six
 import sys
 import time
@@ -31,7 +29,7 @@ from octavia.db import api as db_api
 from octavia.db import models
 from octavia.db import repositories as repo
 from octavia.compute.drivers import nova_driver
-from octavia.network.drivers.neutron import allowed_address_pairs as net_driver
+from octavia.network.drivers.neutron import allowed_address_pairs
 
 LOG = logging.getLogger(__name__)
 
@@ -54,41 +52,55 @@ def get_spare_amphoras(session):
     return amps
 
 
-def delete_amp(nova_manager, session, amp_repo, amp):
-    LOG.debug("Delete vm: %s and amp: %s.", amp.compute_id, amp.id)
+def delete_amp(nova_manager, session, amp_repo, amphealth_repo, amp):
+    LOG.debug("Deleting vm: %s, amp: %s.", amp.compute_id, amp.id)
     if not CONF.dryrun:
-        nova_manager.delete(amp.compute_id)
+        amphealth_repo.delete(session, amphora_id=amp.id)
         amp_repo.delete(session, id=amp.id)
+        nova_manager.delete(amp.compute_id)
 
 
 def check_spare_pool(amp_repo, session):
-    LOG.debug("Waiting for spare pool to fill up...")
-    retries = 10
+    retries = 90
 
     for a in six.moves.xrange(retries):
         conf_spare_cnt = CONF.house_keeping.spare_amphora_pool_size
-        curr_spare_cnt = amp_repo.get_spare_amphora_count(session)
-        LOG.debug(
-            "Required Spare Amphora count: %d, "
-            "Current Spare Amphora count: %d",
-            conf_spare_cnt,
-            curr_spare_cnt)
+        curr_spare_amps = get_spare_amphoras(session)
+        LOG.debug("Required spare amphora count: %d, Current spare amphora "
+                  "count: %d", conf_spare_cnt, len(curr_spare_amps))
 
-        if curr_spare_cnt < conf_spare_cnt:
+        if len(curr_spare_amps) < conf_spare_cnt:
+            if a >= retries:
+                raise Exception("Timeout when waiting for spare pool to "
+                                "fill up.")
+            time.sleep(10)
+        else:
+            amp_ids = [amp.id for amp in curr_spare_amps]
+            with session.begin(subtransactions=True):
+                amp_health_cnt = session.query(models.AmphoraHealth).filter(
+                    models.AmphoraHealth.amphora_id.in_(amp_ids)).count()
+
+            if amp_health_cnt >= len(curr_spare_amps):
+                return
+
             if a >= retries:
                 raise Exception(
                     "Timeout when waiting for spare pool to fill up."
                 )
+
+            LOG.debug(
+                "Waiting for healthcheck of amphorae vms in spare pool, "
+                "%s left.", len(curr_spare_amps) - amp_health_cnt)
+
             time.sleep(10)
-        else:
-            return
 
 
 def get_amps(session, lbid_list=[], role=constants.ROLE_BACKUP):
     with session.begin(subtransactions=True):
         criterion = sa.and_(
             models.Amphora.status == constants.AMPHORA_ALLOCATED,
-            models.Amphora.role == role
+            models.Amphora.role == role,
+            models.Amphora.vrrp_interface != None
         )
 
         if lbid_list:
@@ -105,26 +117,27 @@ def get_amps(session, lbid_list=[], role=constants.ROLE_BACKUP):
 def wait_amps_failover(session, lb_amp_dict, role=constants.ROLE_BACKUP):
     LOG.debug("Waiting for %s amps failover...", role)
 
-    if CONF.dryrun:
-        return
-
-    retries = 10
+    retries = 180
 
     for a in six.moves.xrange(retries):
         lbid_list = list(lb_amp_dict)
         amps = get_amps(session, lbid_list, role)
+        LOG.debug("%s amphorae vms ready.", len(amps))
 
         for amp in amps:
             if amp.id != lb_amp_dict[amp.load_balancer_id]:
                 lb_amp_dict.pop(amp.load_balancer_id)
-                LOG.debug("Failover of %s amp vm for loadbalancer %s is "
-                          "finished. Still have %d left.", role,
-                          amp.load_balancer_id, len(lb_amp_dict))
+
+                LOG.debug("The %s amp vm for loadbalancer %s is created. "
+                          "Still have %s left.", role, amp.load_balancer_id,
+                          len(lb_amp_dict))
 
         if lb_amp_dict:
             if a >= retries:
+                if CONF.dryrun:
+                    return
                 raise Exception("Timeout when waiting for amps failover.")
-            time.sleep(5)
+            time.sleep(10)
         else:
             return
 
@@ -135,16 +148,16 @@ def get_vm_mgmt_port(net_driver, vmid, lb_net_ip):
     for interface in interfaces:
         for ip in interface.fixed_ips:
             if ip.ip_address == lb_net_ip:
-                LOG.debug("Found management port %s for vm %s.",
+                LOG.debug("\tFound management port %s for vm %s.",
                           interface.port_id, vmid)
                 return interface
 
-    raise Exception("VM %s has no port on lb management network.", vmid)
+    raise Exception("\tVM %s has no port on lb management network.", vmid)
 
 
 def disable_port(net_driver, port):
     try:
-        LOG.debug("Disable port %s.", port.port_id)
+        LOG.debug("\tDisable port %s.", port.port_id)
 
         if not CONF.dryrun:
             net_driver.neutron_client.update_port(
@@ -162,6 +175,9 @@ def upgrade_amps(session, net_driver, role=constants.ROLE_BACKUP):
     amps = get_amps(session, role=role)
 
     for amp in amps:
+        LOG.debug("[%s] Processing %s amphorae: %s, vm: %s",
+                  amp.load_balancer_id, role, amp.id, amp.compute_id)
+
         lb_amp_dict[amp.load_balancer_id] = amp.id
         mgmt_port = get_vm_mgmt_port(net_driver, amp.compute_id,
                                      amp.lb_network_ip)
@@ -177,35 +193,39 @@ if __name__ == '__main__':
     service.prepare_service(sys.argv)
     session = db_api.get_session()
     amp_repo = repo.AmphoraRepository()
+    amphealth_repo = repo.AmphoraHealthRepository()
     nova_manager = nova_driver.VirtualMachineManager()
-    neutron_manager = net_driver.AllowedAddressPairsDriver()
-    delete_amp_func = functools.partial(delete_amp, nova_manager, session,
-                                        amp_repo)
-    pool = ThreadPool(4)
+    net_driver = allowed_address_pairs.AllowedAddressPairsDriver()
 
     # Delete all the amp vms in spare pool and corresponding db records.
     amps = get_spare_amphoras(session)
-    LOG.debug("There are %s amphorae vms in spare pool.", len(amps))
+    LOG.debug("Step 1: Clean up %d amphorae vms in spare pool.", len(amps))
 
-    pool.map(delete_amp_func, amps)
+    for amp in amps:
+        delete_amp(nova_manager, session, amp_repo, amphealth_repo, amp)
 
     raw_input('Now, please start octavia house_keeping process, then press '
               'any key to continue...')
 
     # Wait for filling up the spare pool.
+    LOG.debug("Step 2: Waiting for spare pool to fill up again...")
     check_spare_pool(amp_repo, session)
 
     # Find all slave amp vms of all loadbalancers, mark down their management
     # port. The fail over process could be triggered automatically.
-    LOG.debug("Begin to upgrade backup amphorae vms.")
+    LOG.debug("Step 3: Begin to upgrade backup amphorae vms.")
     lb_amp_mapping = upgrade_amps(session, net_driver, constants.ROLE_BACKUP)
     wait_amps_failover(session, lb_amp_mapping, constants.ROLE_BACKUP)
 
+    # TODO: Down the VIP port to trigger VRRP failover, so we can reduce the
+    # interrupted time of VIP connection.
+    LOG.debug("Sleep 60s before handling master amphorae vms...")
+    time.sleep(60)
+
     # Find all master amp vms of all loadbalancers, mark down their management
     # port. The fail over process could be triggered automatically.
-    LOG.debug("Begin to upgrade master amphorae vms.")
+    LOG.debug("Step 4: Begin to upgrade master amphorae vms.")
     lb_amp_mapping = upgrade_amps(session, net_driver, constants.ROLE_MASTER)
     wait_amps_failover(session, lb_amp_mapping, constants.ROLE_MASTER)
 
-    pool.close()
-
+    LOG.debug("Complete upgrading amphorae vms!")
